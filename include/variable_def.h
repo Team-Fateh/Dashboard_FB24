1497ca3dda7183cd94baefdde4f9213887e44c42
pratham-2004 <118990719+pratham-2004@users.noreply.github.com>
2023-12-24 17:31:43 +0530

    Update variable_def.h

--- include/variable_def.h
+++ include/variable_def.h
@@ -1,3 +1,4 @@
+
 #ifndef VARIABLE_H
 #define VARIABLE_H
 #include <FlexCAN_T4.h>
@@ -12,7 +13,6 @@
 #endif
 
 // ***CAN variables***
-
 #define can_data_rate 400
 int64_t can_last_time = 0;
 FlexCAN_T4<CAN2, RX_SIZE_256, TX_SIZE_16> can2;
@@ -32,15 +32,12 @@ int _rxIndex;
 uint8_t _rxData[8];
 
 //***Gear variables***
-
 char gear;
 
 //***Xbee***
-
 unsigned long xbeeLastTime=0,xbeeTIme=50;
 
 //***IMU_MPU6050***
-
 #define OUTPUT_READABLE_ACCELGYRO
 MPU6050 accelgyro;
 int16_t ax, ay, az;
@@ -62,9 +59,6 @@ float fay;
 float faz;
 
 //**loadcell**
-
-// HX711 circuit wiring
-
 File datafile=SD.open("LC_Data.csv",FILE_WRITE);
 // HX711 circuit wiring
 #define LC_DT_FL 27
@@ -97,15 +91,13 @@ float calibrationfactor_FL=0;
 float calibrationfactor_RR=0;
 float calibrationfactor_RL=0;
 
-unsigned long LC_time;
+unsigned long LC_time; 
 
-
-
-//hmi
+//**hmi**
 #define hmi_data_rate 200
 long unsigned int hmi_last_time=0;
 
-//rpm led
+//**rpm_led**
 #define num_led 18
 #define ledPin 29
 #define rpm_data_rate 100
@@ -117,33 +109,49 @@ WS2812Serial leds(num_led, displayMemory, drawingMemory, ledPin, WS2812_GRB);
 #define RED    0xFF0000
 #define GREEN  0x00FF00
 #define BLUE   0x0000FF
-#define WHITE  0x101010
 int light=0;
 int ledDur,ledOldDur,red;
 char comm;
 
-//speed
+//**speed**
 volatile unsigned int totalCounts;
-    #define speedPin 34    //front right
-    #define slits 28   //Front wheel
-    int lastTime=0;      
-    float Speed;      
+#define speedPin 34    //front right
+#define slits 28   //Front wheel
+int lastTime=0;      
+float Speed;      
 
-//speed_rpm
+//**speed_rpm**
 #define fdr 3.118    //Final Drive Rtaio
-    #define pgr 2.667    //Primary Gear Ratio
-    #define gr1 2.67
-    #define gr2 1.86
-    #define gr3 1.42
-    #define gr4 1.14
-    #define gr5 0.96
-    #define gr6 0.84
-    #define td 49.022    //Tire dia of Housier 19.3 inches
-    float SpeedRPM = 0.0;
-    float wheelRPM = 0.0;
+#define pgr 2.667    //Primary Gear Ratio
+#define gr1 2.67
+#define gr2 1.86
+#define gr3 1.42
+#define gr4 1.14
+#define gr5 0.96
+#define gr6 0.84
+#define td 49.022    //Tire dia of Housier 19.3 inches
+float SpeedRPM = 0.0;
+float wheelRPM = 0.0;
 
-
-
-
-
+//**SD_card**
+File myFile;
+const int chipSelect = BUILTIN_SDCARD;
+String f;
+String name = "/Data";
+String ext= ".csv";
+String mystr;
+String eeprom_val ;
+int i=0;
+int sd_pin = 28;//Data_switch_pin
+int count = 0;
+int vari =0;
+int sv=0;
+int tempo;
+const char * file_name;
+const char* n;
+const char* input;
+char EEPROM_val;
+int filecreate = 0;
+int datalog =0;
 #endif
+
